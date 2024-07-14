b8ed75c42106af080cb051ad348f349daeeb7b21
Brijesh Vekariya <125974636+brijeshvek@users.noreply.github.com>
2024-07-14 10:45:28 +0530

    Update action.js

--- Counter/src/Redux/action.js
+++ Counter/src/Redux/action.js
@@ -1,14 +1,11 @@
 
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
-export const PURCHASE_BOOK = 'PURCHASE_BOOK';
+
 
 export const increment = ()=>{
     return {type:INCREMENT}
 }
 export const decrement = () =>{
     return {type:DECREMENT}
-}
-export const parchesh_book = () =>{
-    return {type :PURCHASE_BOOK}
 }
