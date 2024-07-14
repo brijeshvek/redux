188eca74f81722edc6267aead16e37dd079b735a
Brijesh Vekariya <125974636+brijeshvek@users.noreply.github.com>
2024-07-14 10:45:11 +0530

    Update CouterComponents.jsx

--- Counter/src/CouterComponents.jsx
+++ Counter/src/CouterComponents.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { INCREMENT ,DECREMENT, PURCHASE_BOOK } from './Redux/action'
+import { INCREMENT ,DECREMENT } from './Redux/action'
 
 function CouterComponents() {
     const count = useSelector((state)=>state.count)
-    const books = useSelector((state)=>state.books)
+
     const dispatch =useDispatch()
   return (
     <div>
@@ -12,9 +12,6 @@ function CouterComponents() {
         <h4>couter is: {count} </h4>
         <button onClick={()=>dispatch({type:INCREMENT})}>Add</button>
         <button onClick={()=>dispatch({type:DECREMENT})}>Minus</button>
-        <br />
-        <h3>Book Parchesh is :{books}</h3>
-        <button onClick={()=>dispatch({type:PURCHASE_BOOK})}>Purchase Book</button>
     </div>
   )
 }
